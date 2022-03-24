b731ba58d3290e33d3438f4b92c01cc37801e438
yan <yan_1731@hotmail.com>
2022-03-23 20:10:39 -0700

    Fixed a couple of useless variables

--- libft.h
+++ libft.h
@@ -15,6 +15,12 @@
 # include	<unistd.h>
 # include	<stdlib.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -58,11 +64,5 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
 
 #endif
